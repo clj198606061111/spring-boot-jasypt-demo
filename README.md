0a8d4f5068d6cd9766e29e5009f85d3a36e12e8c
clj198606061111@163.com <clj-1986>
2020-09-13 15:31:54 +0800

    从外部配置文件获取参数

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # spring-boot-jasypt-demo
 spring-boot-jasypt-demo
+
+## 启动参数指定外部配置文件
+
+-Dspring.config.location=D:\workspace_idea\spring-boot-jasypt-demo\docs\itclj-sit.properties
+
+## 通过接口获取参数
+
+```shell script
+http://127.0.0.1:8080/itclj/param1
+```
+
+
+
---
+++ src/main/java/com/itclj/controller/ItcljController.java
@@ -0,0 +1,19 @@
+package com.itclj.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/itclj")
+public class ItcljController {
+
+    @Value("${itclj.param1}")
+    private String parma1;
+
+    @GetMapping("/param1")
+    public String getParma1(){
+        return parma1;
+    }
+}
