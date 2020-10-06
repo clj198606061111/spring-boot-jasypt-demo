18dd96f43775132785579b33ebaaabc16c8385d9
clj198606061111@163.com <clj-1986>
2020-10-06 17:24:03 +0800

    加密秘钥放到程序代码中

--- src/main/java/com/itclj/ItcljApplication.java
+++ src/main/java/com/itclj/ItcljApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ItcljApplication {
     public static void main(String[] args) {
+
+        System.setProperty("jasypt.encryptor.password","itclj@2020");
+
         SpringApplication.run(ItcljApplication.class,args);
     }
 }
